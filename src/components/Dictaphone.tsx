da58a37855afbd06362ca0941bf4f9f8e7ebcae0
dokimion24 <leecy3610@gmail.com>
2023-10-10 21:23:43 +0900

    feat: add speech to text component

---
+++ src/components/Dictaphone.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import 'regenerator-runtime/runtime';
+import React from 'react';
+import { createSpeechlySpeechRecognition } from '@speechly/speech-recognition-polyfill';
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from 'react-speech-recognition';
+
+// const appId = '<INSERT_SPEECHLY_APP_ID_HERE>';
+// const SpeechlySpeechRecognition = createSpeechlySpeechRecognition(appId);
+// SpeechRecognition.applyPolyfill(SpeechlySpeechRecognition);
+
+const Dictaphone = () => {
+  const {
+    transcript,
+    listening,
+    browserSupportsSpeechRecognition,
+    resetTranscript,
+  } = useSpeechRecognition();
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true });
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesnt support speech recognition.</span>;
+  }
+
+  console.log(transcript);
+
+  return (
+    <div>
+      <p>Microphone: {listening ? 'on' : 'off'}</p>
+      <button onClick={startListening}>Start</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
+    </div>
+  );
+};
+export default Dictaphone;
